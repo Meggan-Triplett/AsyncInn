dee8f21e6e70045b929fb940c1f40a2c2b52914a
Meggan <meggtriplett@gmail.com>
2019-06-27 11:24:53 -0700

    Home page routing complete and will display text

--- AsyncInn/AsyncInn/Startup.cs
+++ AsyncInn/AsyncInn/Startup.cs
@@ -15,7 +15,7 @@ namespace AsyncInn
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddMvc();
 
         }
 
@@ -27,10 +27,14 @@ namespace AsyncInn
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+           {
+               routes.MapRoute(
+               name: "default",
+               template: "{controller=Home}/{action=Index}/{id?}");
+           });
         }
     }
 }
