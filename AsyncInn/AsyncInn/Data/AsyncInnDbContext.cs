5aff35def4080b8a19573a37dbef3337f1d081eb
Meggan <meggtriplett@gmail.com>
2019-06-27 12:06:51 -0700

    Initial database creation setup

---
+++ AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AsyncInn.Data
+{
+    public class AsyncInnDBContext: DbContext
+    {
+        public AsyncInnDBContext(DbContextOptions<AsyncInnDBContext> options) : base 
+            (options)
+        {
+
+        }
+    }
+}
